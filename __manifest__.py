8ff74db1a1c25771305ccf2774466172f36c7e4c
Iago Alonso <iago.alonso@alliantum.com>
2022-05-30 16:20:10 +0200

    [IMP] Use the new readme format

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     """,
     'author': "Alliantum",
     'website': "https://www.alliantum.com",
-    'version': '14.0.1.0.2',
+    'version': '14.0.1.0.3',
     'license': 'AGPL-3',
     'category': 'Sales',
     'depends': ['base', 'sale', 'sale_stock'],
